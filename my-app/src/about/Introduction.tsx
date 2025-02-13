d5d091af98883bee413fad8957d27b7209bf0f3f
Biswajit <paridabiswa2k@gmail.com>
2025-02-13 19:14:22 +0530

    adding introductions

---
+++ my-app/src/about/Introduction.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Introduction(){
+    return(
+        <>
+        <h1>Hi I am Biswajit parida the great</h1>
+        <h1>i work  like rothschild</h1>
+        <p>i am the next big thing in india</p>
+        </>
+    )
+}
+
+export default Introduction
