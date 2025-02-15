5f829fe8d4f47429bcc1e28211be212bafd4ed3b
Biswajit <paridabiswa2k@gmail.com>
2025-02-16 05:08:18 +0530

    adding details

---
+++ my-app/src/object/ProjectObject.tsx
@@ -0,0 +1,12 @@
+
+import snap from '../assets/Screenshot (90).png'
+const projectArray=[
+    {
+        id:1,
+        name:'E-commerce site',
+        descriprion:'a full stack ecommerce site build with react ,nodejs,express and mongodb',
+        image:snap,
+        liveLink:
+
+    }
+]
